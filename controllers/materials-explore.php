b9c5007337ee8f2f9422553ecd3b7b0b1c53ce28
Veliona Vollerei <veliona.vollerei@gmail.com>
2021-03-11 16:38:26 +0100

    Create base for the page

---
+++ controllers/materials-explore.php
@@ -0,0 +1,16 @@
+<?php
+include "models/material.php";
+
+$material_DB = new material($dbh);
+
+$list_material_type = $material_DB->get_material_types();
+
+var_dump($params);
+if(isset($params[2])){
+    $id_selected_mat_type = $params[2];
+}
+
+var_dump($id_selected_mat_type);
+echo $id_selected_mat_type;
+
+include "vue/materials-explore.php";
