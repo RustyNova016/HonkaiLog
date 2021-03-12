8926902c1a709c1d3223ab3473b72d32a2642782
Veliona Vollerei <veliona.vollerei@gmail.com>
2021-03-12 22:16:03 +0100

    function request_bp()

--- models/bp.php
+++ models/bp.php
@@ -1,11 +1,23 @@
 <?php
 
 
-class bp
-{
+class bp{
     private static $dbh;
 
     public function __construct($myDbh){
-        material::$dbh = $myDbh;
+        bp::$dbh = $myDbh;
+    }
+
+    private function request_bp(){
+        $SQLrequest = "SELECT xp_count, lv_max_F, duration, date_end    
+                       FROM bp_season_progress 
+                       INNER JOIN bp_season ON bp_season_progress.id_season = bp_season.id
+                       WHERE id_user = :id_user 
+                       	AND bp_season.date_end > DATE_ADD(CURRENT_TIMESTAMP(), INTERVAL 4 HOUR)
+                       ORDER BY time_stamp;";
+
+        $sth = bp::$dbh->prepare($SQLrequest);
+        $sth->execute([$_SESSION["iduser"]]);
+        return $sth->fetchall();
     }
 }
