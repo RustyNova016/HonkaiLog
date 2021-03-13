2f59b025320272ad4ef4f6761835135825b17ce4
Veliona Vollerei <veliona.vollerei@gmail.com>
2021-03-13 19:20:31 +0100

    Switch to private variables

--- models/bp.php
+++ models/bp.php
@@ -2,17 +2,18 @@
 include "models/date_function.php";
 
 class bp{
-    private static $dbh;
-    public static $bp_info;
-    public static $total_days;
-    public static $days_left;
+    private $dbh;
+    private $bp_info;
+    private $total_days;
+    private $days_left;
 
     public function __construct($myDbh){
-        bp::$dbh = $myDbh;
-        bp::$bp_info = $this->request_bp();
-        bp::$total_days = diff_whole_days(bp::$bp_info[0]["date_start"], bp::$bp_info[0]["date_end"]);
+        // Database requests
+        $this->dbh = $myDbh;
+        $this->bp_info = $this->request_bp();
+        $this->total_days = diff_whole_days($this->$bp_info[0]["date_start"], $this->$bp_info[0]["date_end"]);
         $date = new DateTime();
-        bp::$days_left = diff_whole_days($date->format('Y-m-d H:i:s'), bp::$bp_info[0]["date_end"]);
+        $this->days_left = diff_whole_days($date->format('Y-m-d H:i:s'), $this->$bp_info[0]["date_end"]);
 
         var_dump(bp::$total_days);
         var_dump(bp::$days_left);
@@ -24,8 +25,9 @@ class bp{
                        FROM bp_season_progress 
                        INNER JOIN bp_season ON bp_season_progress.id_season = bp_season.id
                        WHERE id_user = :id_user 
-                       	AND bp_season.date_end > DATE_ADD(CURRENT_TIMESTAMP(), INTERVAL 4 HOUR)
-                       ORDER BY time_stamp;";
+                           AND bp_season.date_end > DATE_ADD(CURRENT_TIMESTAMP(), INTERVAL 4 HOUR)
+                       ORDER BY time_stamp DESC
+                       LIMIT 1;";
 
 
 
