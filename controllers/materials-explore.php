a4cbe482b72a9aba740e70ad7c47a496ca8bd193
Veliona Vollerei <veliona.vollerei@gmail.com>
2021-04-02 14:49:15 +0200

    Reformat indents 2

--- controllers/materials-explore.php
+++ controllers/materials-explore.php
@@ -1,101 +1,99 @@
 <?php
-include "models/material_db.php";
-
-$material_DB = new material_db($dbh);
-
-$list_material_type = $material_DB->get_material_types();
-
-if(isset($params[2])){
-    $id_selected_mat_type = $params[2];
+    include "models/material_db.php";
     
-    $selected_mat_type = $material_DB->get_material_type_info($id_selected_mat_type)[0];
-
-    $list_materials = $material_DB->get_material_list_of_type($id_selected_mat_type);
-}
-
-
-$timespan_type = [
-    [
-        "name" => "Today",
-        "SQL" => "0 DAY",
-        "start" => "Today",
-        "nbr_day" => 1,
-        "wholeday" => 1
-    ],
-    [
-        "name" => "Last 24h",
-        "SQL" => "1 DAY",
-        "start" => "In the last 24h",
-        "nbr_day" => 1,
-        "wholeday" => 0
-    ],
-    [
-        "name" => "Last 7 day",
-        "SQL" => "7 DAY",
-        "start" => "In the last 7 days",
-        "nbr_day" => 7,
-        "wholeday" => 1
-    ],
-    [
-        "name" => "Last 30 day",
-        "SQL" => "30 DAY",
-        "start" => "In the last 30 days",
-        "nbr_day" => 30,
-        "wholeday" => 1
-    ],
-    [
-        "name" => "Last year",
-        "SQL" => "365 DAY",
-        "start" => "In the last year",
-        "nbr_day" => 365,
-        "wholeday" => 1
-    ]
-];
+    $material_DB = new material_db($dbh);
+    
+    $list_material_type = $material_DB->get_material_types();
+    
+    if (isset($params[2])) {
+        $id_selected_mat_type = $params[2];
+        
+        $selected_mat_type = $material_DB->get_material_type_info($id_selected_mat_type)[0];
+        
+        $list_materials = $material_DB->get_material_list_of_type($id_selected_mat_type);
+    }
+    
+    
+    $timespan_type = [
+        [
+            "name" => "Today",
+            "SQL" => "0 DAY",
+            "start" => "Today",
+            "nbr_day" => 1,
+            "wholeday" => 1
+        ],
+        [
+            "name" => "Last 24h",
+            "SQL" => "1 DAY",
+            "start" => "In the last 24h",
+            "nbr_day" => 1,
+            "wholeday" => 0
+        ],
+        [
+            "name" => "Last 7 day",
+            "SQL" => "7 DAY",
+            "start" => "In the last 7 days",
+            "nbr_day" => 7,
+            "wholeday" => 1
+        ],
+        [
+            "name" => "Last 30 day",
+            "SQL" => "30 DAY",
+            "start" => "In the last 30 days",
+            "nbr_day" => 30,
+            "wholeday" => 1
+        ],
+        [
+            "name" => "Last year",
+            "SQL" => "365 DAY",
+            "start" => "In the last year",
+            "nbr_day" => 365,
+            "wholeday" => 1
+        ]
+    ];
 
 //var_dump($list_materials);
-
-
-include "models/material/material.php";
-include "models/material/material_history.php";
-include "models/material/time_frame.php";
-
-$time_frame_list = [];
-
-foreach ($timespan_type as $time_array){
-    $new_time_frame = new time_frame(
-        $time_array["SQL"],
-        $time_array["name"],
-        $time_array["nbr_day"],
-        $time_array["wholeday"],
-        $time_array["start"],
-        $time_array["name"]
-    );
-    //var_dump($new_time_frame);
-    array_push($time_frame_list, $new_time_frame);
-}
+    
+    
+    include "models/material/material.php";
+    include "models/material/material_history.php";
+    include "models/material/time_frame.php";
+    
+    $time_frame_list = [];
+    
+    foreach ($timespan_type as $time_array) {
+        $new_time_frame = new time_frame(
+            $time_array["SQL"],
+            $time_array["name"],
+            $time_array["nbr_day"],
+            $time_array["wholeday"],
+            $time_array["start"],
+            $time_array["name"]
+        );
+        //var_dump($new_time_frame);
+        array_push($time_frame_list, $new_time_frame);
+    }
 
 //var_dump($time_frame_list);
-
-$material_list_ = [];
-foreach($list_materials as $material_base){
-    $material = new material($material_base["id_material"], $material_base["name"]);
-
-
-
-    if (!empty($_POST[$material->getId()."_quantity"])){
-        $material->log_material_count($dbh, $_POST[$material->getId()."_quantity"], "");
+    
+    $material_list_ = [];
+    foreach ($list_materials as $material_base) {
+        $material = new material($material_base["id_material"], $material_base["name"]);
+        
+        
+        if (!empty($_POST[$material->getId() . "_quantity"])) {
+            $material->log_material_count($dbh, $_POST[$material->getId() . "_quantity"], "");
+        }
+        //var_dump($material);
+        
+        
+        foreach ($time_frame_list as $one_time_frame) {
+            $material->request_material_history($dbh, $one_time_frame);
+        }
+        
+        
+        array_push($material_list_, $material);
     }
-    //var_dump($material);
-
-
-    foreach($time_frame_list as $one_time_frame){
-        $material->request_material_history($dbh, $one_time_frame);
-    }
-
-
-
-    array_push($material_list_, $material);
-}
 //var_dump($material_list_);
-$_POST = array();
-include "vue/materials-explore.php";
+    $_POST = array();
+    include "vue/materials-explore.php";
--- models/material/material_history.php
+++ models/material/material_history.php
@@ -1,6 +1,6 @@
 <?php
-
-
+    
+    
     class material_history {
         private $date_start;
         private $date_end;
@@ -8,31 +8,31 @@
         private $net_gains;
         private $net_loss;
         private time_frame $time_frame;
-    
-        public function __construct($timestamps, $time_frame){
+        
+        public function __construct($timestamps, $time_frame) {
             $this->timestamps = $timestamps;
             $this->time_frame = $time_frame;
         }
-    
-    
-        public function get_current_count() : int {
+        
+        
+        public function get_current_count(): int {
             return $this->get_latest_timestamp()["quantity"];
         }
-    
-        public function get_oldest_count(){
+        
+        public function get_oldest_count() {
             return $this->get_oldest_timestamp()["quantity"];
         }
-    
-        public function get_oldest_timestamp(){
+        
+        public function get_oldest_timestamp() {
             return $this->timestamps[0];
         }
-    
-        public function get_latest_timestamp(){
+        
+        public function get_latest_timestamp() {
             return end($this->timestamps);
         }
-    
-        public function get_average_change(){
-            if ($this->time_frame->getNbrDay() > 1){
+        
+        public function get_average_change() {
+            if ($this->time_frame->getNbrDay() > 1) {
                 $overall_change_average = round($this->get_overall_change() / $this->time_frame->getNbrDay(), 2);
             } else {
                 $overall_change_average = -1;
@@ -40,9 +40,9 @@
             //var_dump($overall_change_average);
             return $overall_change_average;
         }
-    
+        
         public function get_average_gain() {
-            if ($this->time_frame->getNbrDay() > 1){
+            if ($this->time_frame->getNbrDay() > 1) {
                 $gain_average = round($this->getNetGains() / $this->time_frame->getNbrDay(), 2);
             } else {
                 $gain_average = -1;
@@ -52,7 +52,7 @@
         }
         
         public function get_average_loss() {
-            if ($this->time_frame->getNbrDay() > 1){
+            if ($this->time_frame->getNbrDay() > 1) {
                 $gain_loss = round($this->getNetLoss() / $this->time_frame->getNbrDay(), 2);
             } else {
                 $gain_loss = -1;
@@ -61,66 +61,62 @@
             return $gain_loss;
         }
         
-        public function get_overall_change(){
+        public function get_overall_change() {
             return $this->get_current_count() - $this->get_oldest_count();
         }
-    
+        
         /** Calculate gains and losses
          *
          */
-        private function gain_loss(){
+        private function gain_loss() {
             $this->net_gains = 0;
             $this->net_loss = 0;
             $old_amount = $this->timestamps[0]["quantity"];
-            for ($i=1; $i < count($this->timestamps); $i++) {
+            for ($i = 1; $i < count($this->timestamps); $i++) {
                 $amount = $this->timestamps[$i]["quantity"];
                 $diff = $amount - $old_amount;
-    
-                if ($diff > 0){
+                
+                if ($diff > 0) {
                     $this->net_gains += $diff;
-                } elseif ($diff < 0){
+                } elseif ($diff < 0) {
                     $this->net_loss += $diff;
                 }
-    
+                
                 $old_amount = $amount;
             }
         }
-    
+        
         /**
          * @return DateTime
          */
-        public function getDateEnd(): DateTime
-        {
+        public function getDateEnd(): DateTime {
             return $this->date_end;
         }
-    
+        
         /**
          * @return int
          */
-        public function getNetGains(): int
-        {
-            if (empty($this->net_gains)){
+        public function getNetGains(): int {
+            if (empty($this->net_gains)) {
                 $this->gain_loss();
             }
             return $this->net_gains;
         }
-    
+        
         /**
          * @return int
          */
-        public function getNetLoss(): int
-        {
-            if (empty($this->net_loss)){
+        public function getNetLoss(): int {
+            if (empty($this->net_loss)) {
                 $this->gain_loss();
             }
             return $this->net_loss;
         }
-    
+        
         /**
          * @return mixed
          */
-        public function getTimeFrame()
-        {
+        public function getTimeFrame() {
             return $this->time_frame;
         }
     }
