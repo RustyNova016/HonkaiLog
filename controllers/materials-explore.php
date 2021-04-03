2b07e842c3c5eb0125c07a564f34d9c002c9278d
Veliona Vollerei <veliona.vollerei@gmail.com>
2021-04-03 14:23:25 +0200

    Material type viewer

--- controllers/materials-explore.php
+++ controllers/materials-explore.php
@@ -1,18 +1,33 @@
 <?php
-    include "models/material_db.php";
+    require_once "models/material_db.php";
+    require_once "models/material/material_type.php";
+    require_once "models/material/find_material_type_child.php";
+    require_once "models/material/material.php";
+    require_once "models/material/material_history.php";
+    require_once "models/material/time_frame.php";
     
+    // We get the list of the ids of the materials
     $material_DB = new material_db($dbh);
+    $list_of_id_material_type = $material_DB->get_material_types();
     
-    $list_material_type = $material_DB->get_material_types();
-    
-    if (isset($params[2])) {
-        $id_selected_mat_type = $params[2];
-        
-        $selected_mat_type = $material_DB->get_material_type_info($id_selected_mat_type)[0];
-        
-        $list_materials = $material_DB->get_material_list_of_type($id_selected_mat_type);
+    // We then transform the ids into material_type objects
+    $list_of_material_type = [];
+    foreach ($list_of_id_material_type as $material_type) {
+        $list_of_material_type[$material_type["id_material_type"]] = new material_type($db, $material_type["id_material_type"]);
     }
     
+    // Now, we prepare the material list
+    $hierachical_material_types_id = find_material_type_child(1, $list_of_material_type);
+    
+    // We check if the user already selected a material type
+    if (isset($params[2])) {
+        // If so, we retrieve the id
+        $id_selected_mat_type = $params[2];
+    } else {
+        $id_selected_mat_type = 1;
+    }
+    // Get the children
+    $get_children_id = find_material_type_child($id_selected_mat_type, $list_of_material_type);
     
     $timespan_type = [
         [
@@ -51,13 +66,6 @@
             "wholeday" => 1
         ]
     ];
-
-//var_dump($list_materials);
-    
-    
-    include "models/material/material.php";
-    include "models/material/material_history.php";
-    include "models/material/time_frame.php";
     
     $time_frame_list = [];
     
@@ -73,27 +81,25 @@
         //var_dump($new_time_frame);
         array_push($time_frame_list, $new_time_frame);
     }
-
-//var_dump($time_frame_list);
     
-    $material_list_ = [];
-    foreach ($list_materials as $material_base) {
-        $material = new material($material_base["id_material"], $material_base["name"]);
-        
-        
-        if (!empty($_POST[$material->getId() . "_quantity"])) {
-            $material->log_material_count($dbh, $_POST[$material->getId() . "_quantity"], "");
-        }
-        //var_dump($material);
-        
-        
-        foreach ($time_frame_list as $one_time_frame) {
-            $material->request_material_history($dbh, $one_time_frame);
-        }
-        
-        
-        array_push($material_list_, $material);
+    // We retrieve the material data
+    foreach ($get_children_id as $id_child => $depth) {
+        $list_of_material_type[$id_child]->query_material_list($db, $time_frame_list);
     }
-//var_dump($material_list_);
+    
+    // Did the user sent a new count? Log it
+    if (!empty($_POST)) {
+        foreach ($list_of_material_type as $material_type) {
+            
+            $list_of_material = $material_type->get_list_of_material();
+            foreach ($list_of_material as $material) {
+                
+                if (!empty($_POST[$material->getId() . "_quantity"])) {
+                    $material->log_material_count($dbh, $_POST[$material->getId() . "_quantity"], "");
+                }
+            }
+        }
+        
+    }
     $_POST = array();
     include "vue/materials-explore.php";
--- models/material/find_material_type_child.php
+++ models/material/find_material_type_child.php
@@ -1,16 +1,34 @@
 <?php
-    function find_material_type_child(int $id_current, $material_list){
-        $id_of_children = [];
+    function find_material_type_child(int $id_current, $list_of_material_type){
+        $list_of_child_id = [];
         
-        foreach ($material_list as $material_type) {
-            //var_dump($material_type);
-            //var_dump($material_list);
-            //var_dump($material_list[$id_current]);
-            if ($material_type->get_parent_id() == $material_list[$id_current]->get_id()){
-                $id_of_children = array_merge($id_of_children, find_material_type_child($material_type->get_id(), $material_list));
+        foreach ($list_of_material_type as $material_type) { // for each material of the list
+            
+            // If the material is the child of the target material
+            if ($material_type->get_parent_id() == $list_of_material_type[$id_current]->get_id()){
+                
+                // We add it to the list
+                array_push($list_of_child_id, $material_type->get_id());
             }
         }
         
-        array_push($id_of_children, $material_list[$id_current]->get_id());
-        return $id_of_children;
+        // We set up the depth counter
+        $result_array = [
+            $id_current => 0,
+        ];
+        
+        // For each child id, we get their the ids of their own children
+        foreach ($list_of_child_id as $child_id) {
+            $find_material_type_child = find_material_type_child($child_id, $list_of_material_type);
+            $result_array = $result_array + $find_material_type_child;
+        }
+    
+        // We add +1 to the depth of all the childs
+        $add_depth = [];
+        foreach ($result_array as $id_child => $depth) {
+            $add_depth[$id_child] = $depth + 1;
+        }
+
+        // We return the result
+        return $add_depth;
     }
