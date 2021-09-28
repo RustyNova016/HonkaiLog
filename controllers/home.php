3b08e0e364c7eb88b650d773bcb0f3726f4f5ee6
VelionaVollerei <75586016+VelionaVollerei@users.noreply.github.com>
2021-09-28 11:07:43 +0200

    Controler and home

---
+++ controllers/home.php
@@ -0,0 +1,7 @@
+<?php
+    
+    class home extends controller {
+        public function index() {
+            $this->render("index");
+        }
+    }
--- core/controller.php
+++ core/controller.php
@@ -2,15 +2,30 @@
     
     class controller {
         private array $vars = array();
+        protected string $layout = "default";
         
         /** Display the view
          *
          * @param string $filename
          */
         protected function render(string $filename) {
+            // We send the data to the view
             extract($this->vars);
             
+            // Start the buffer
+            ob_start();
+            
             require ROOT . 'views/' . get_class($this) . "/" . $filename . '.php';
+            
+            // Get the buffer
+            $content_for_layout = ob_get_clean();
+            
+            // if no layout
+            if($this->layout == false){
+                echo $content_for_layout;
+            } else {
+                require ROOT.'views/layouts/'.$this->layout.'.php';
+            }
         }
         
         /** Put data in vars
