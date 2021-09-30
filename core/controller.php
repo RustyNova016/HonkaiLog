e0f8c89fe4c5f2743fb3df8fb95650d66a080988
VelionaVollerei <75586016+VelionaVollerei@users.noreply.github.com>
2021-09-30 19:04:55 +0200

    Redirect controller

--- core/controller.php
+++ core/controller.php
@@ -1,14 +1,33 @@
 <?php
     
+    use JetBrains\PhpStorm\NoReturn;
+    
     class controller {
-        private array $vars = array();
         protected string $layout = "default";
+        protected string $redirect_location;
+        private array $vars = array();
+        
+        public function __construct() {
+            // Redirect check
+            if (isset($this->redirect_location)) {
+                $this->redirect($this->redirect_location);
+            }
+        }
+        
+        /** Redirect somewhere
+         *
+         * @param string $location
+         */
+        #[NoReturn] protected function redirect(string $location): void {
+            header("Location: $location"); //TODO: URL Manipulation check
+            die();
+        }
         
         /** Display the view
          *
          * @param string $filename
          */
-        protected function render(string $filename) {
+        protected function render(string $filename): void {
             // We send the data to the view
             extract($this->vars);
             
@@ -21,10 +40,10 @@
             $content_for_layout = ob_get_clean();
             
             // if no layout
-            if($this->layout == false){
+            if ($this->layout == false) {
                 echo $content_for_layout;
             } else {
-                require ROOT.'views/layouts/'.$this->layout.'.php';
+                require ROOT . 'views/layouts/' . $this->layout . '.php';
             }
         }
         
@@ -32,7 +51,7 @@
          *
          * @param array $d
          */
-        protected function set(array $d) {
+        protected function set(array $d): void {
             $this->vars = array_merge($this->vars, $d);
         }
     }
