17ef53d41c621b370f42d78b2ef648915aa002c0
RustyNova <rusty.nova.jsb@gmail.com>
2022-06-02 14:06:48 +0200

    Added link on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
-import type {NextPage} from 'next'
 import styles from '../styles/Home.module.scss'
+import pageStyles from '../styles/page.module.scss'
 import {GenericPageLayout} from "../component/pageComponents/GenericPageLayout";
 import {Fade} from "react-awesome-reveal";
 import {SmallLinkCard} from "../component/pageComponents/smallLinkCard";
 
 
-const Home: NextPage = () => {
+function Home() {
     return (<>
             <GenericPageLayout pushFooter={true}>
-                <main className={styles.main}>
+                <main className={pageStyles.main}>
 
                     <Fade direction={"down"} cascade={true}>
-                        <h1 className={styles.title}>
+                        <h1 className={pageStyles.title}>
                             <b>Welcome to HonkaiLog!</b>
                         </h1>
 
@@ -20,10 +20,12 @@ const Home: NextPage = () => {
                         </p>
                     </Fade>
                     <Fade direction={"up"} cascade={true} className={styles.grid} delay={1000}>
-                        <SmallLinkCard link={"404"} title={"Material History"}
+                        <SmallLinkCard link={"404"}
+                                       title={"Material History"}
                                        description={"Keep an history of your materials to see how many you are making over a period of time"}
                                        imagelink={"/images/icons/HonkaiCube.png"}/>
-                        <SmallLinkCard link={"404"} title={"Battlepass Checker"}
+                        <SmallLinkCard link={"/battlepass"}
+                                       title={"Battlepass Checker"}
                                        description={"Not sure if you can finish the battlepass in time? Use this tool to find out"}
                                        imagelink={"/images/icons/BP.png"}/>
                     </Fade>
