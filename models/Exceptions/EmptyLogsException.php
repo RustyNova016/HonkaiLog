2fa5e7594de43c6375d58f2493254a4acc448f0f
RustyNova <rusty.nova.jsb@gmail.com>
2022-06-09 18:32:20 +0200

    More deleting 2
    Took 1 minute

--- models/Exceptions/EmptyLogsException.php
+++
@@ -1,8 +0,0 @@
-<?php
-    
-    
-    class EmptyLogsException extends Exception {
-        public function __construct($message = "Empty logs", $code = 0, Throwable $previous = null) {
-            parent::__construct($message, $code, $previous);
-        }
-    }
