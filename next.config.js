049b4d9bbe6aa85fa3dabe980fc4ddb3ac240aa3
RustyNova <rusty.nova.jsb@gmail.com>
2022-11-27 16:16:15 +0100

    Going Next 13

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
-const path = require('path')
-
+const {env} = require("./env");
+const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        appDir: true,
+    },
     reactStrictMode: true,
+    devIndicators: {
+        buildActivityPosition: 'bottom-right',
+        buildActivity: true,
+    },
+    env: env,
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
