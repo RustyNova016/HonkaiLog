d209ad0fc3504203d06a7e234887c7236f36ecfb
RustyNova <rusty.nova.jsb@gmail.com>
2022-12-03 17:43:44 +0100

    Fixed .env bug

--- tools/NextAuth/tools.ts
+++ tools/NextAuth/tools.ts
@@ -1,12 +1,19 @@
 import GitHubProvider from "next-auth/providers/github";
 import {Account, CallbacksOptions, Profile} from "next-auth";
 import {Provider} from "next-auth/providers";
+import {z} from "zod";
 
+const GithubProviderOptions = z.object({
+    clientId: z.string(),
+    clientSecret: z.string()
+})
+
+const options = GithubProviderOptions.parse({
+    clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET
+});
 export const AuthProviders: Provider[] = [
-    GitHubProvider({
-        clientId: process.env.GITHUB_CLIENT_ID,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET
-    })
+    GitHubProvider(options)
 ];
 
 export const AuthCallbacks: Partial<CallbacksOptions<Profile, Account>> = {
