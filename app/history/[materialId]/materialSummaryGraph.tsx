5a71d5d35ca16ff240a86f5f68223e8eff77f33c
RustyNova <rusty.nova.jsb@gmail.com>
2023-01-17 22:10:57 +0100

    Graph working

--- app/history/[materialId]/materialSummaryGraph.tsx
+++ app/history/[materialId]/materialSummaryGraph.tsx
@@ -1,37 +1,89 @@
 "use client"
-import CanvasJSReact from '@/lib/CanvasJS/canvasjs.react';
-var CanvasJS = CanvasJSReact.CanvasJS;
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+import {useEffect, useState} from "react";
 
 export function MaterialSummaryGraph() {
     //const options = {
     //    theme: "dark2",
     //    animationEnabled: true
     //}
+    const [showGraph, setShowGraph] = useState(false);
+    // Detect if the component is on the client
+    useEffect(() => {
+        console.log("using the effect");
+        if (typeof window === 'object') {
+            console.log("We have a window!");
+            // Check if document is finally loaded
+            setShowGraph(true)
+        }
+    }, []);
+
+
+    if (!showGraph) {return <></>}
+
+    const {CanvasJSChart, CanvasJS} = require('@/lib/CanvasJS/canvasjs.react');
+    const CanvasJSReact = require('@/lib/CanvasJS/canvasjs.react');
+    console.log("CanvasJSReact", CanvasJSReact)
+
+    console.log("CanvasJS as dot", CanvasJSReact.CanvasJS)
+    console.log("CanvasJS", CanvasJS)
+
+    console.log("CanvasJSChart", CanvasJSChart)
 
     const options = {
-			animationEnabled: true,
-			exportEnabled: true,
-			theme: "dark2", // "light1", "dark1", "dark2"
-			title:{
-				text: "Trip Expenses"
-			},
-			data: [{
-				type: "pie",
-				indexLabel: "{label}: {y}%",
-				startAngle: -90,
-				dataPoints: [
-					{ y: 20, label: "Airfare" },
-					{ y: 24, label: "Food & Drinks" },
-					{ y: 20, label: "Accomodation" },
-					{ y: 14, label: "Transportation" },
-					{ y: 12, label: "Activities" },
-					{ y: 10, label: "Misc" }
-				]
-			}]
-		}
+        animationEnabled: true,
+        exportEnabled: true,
+        theme: "light2", // "light1", "dark1", "dark2"
+        title: {
+            text: "Bounce Rate by Week of Year"
+        },
+        axisY: {
+            title: "Bounce Rate",
+            includeZero: false,
+            suffix: "%"
+        },
+        axisX: {
+            title: "Week of Year",
+            prefix: "W",
+            interval: 2
+        },
+        data: [{
+            type: "line",
+            toolTipContent: "Week {x}: {y}%",
+            dataPoints: [
+                {x: 1, y: 64},
+                {x: 2, y: 61},
+                {x: 3, y: 64},
+                {x: 4, y: 62},
+                {x: 5, y: 64},
+                {x: 6, y: 60},
+                {x: 7, y: 58},
+                {x: 8, y: 59},
+                {x: 9, y: 53},
+                {x: 10, y: 54},
+                {x: 11, y: 61},
+                {x: 12, y: 60},
+                {x: 13, y: 55},
+                {x: 14, y: 60},
+                {x: 15, y: 56},
+                {x: 16, y: 60},
+                {x: 17, y: 59.5},
+                {x: 18, y: 63},
+                {x: 19, y: 58},
+                {x: 20, y: 54},
+                {x: 21, y: 59},
+                {x: 22, y: 64},
+                {x: 23, y: 59}
+            ]
+        }]
+    }
 
-    return <div>
-        <CanvasJSChart option={options}/>
-    </div>;
+    return (
+        <div>
+            <h1>React Line Chart</h1>
+            <CanvasJSChart options={options}
+                /* onRef={ref => this.chart = ref} */
+            />
+            {/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+        </div>
+    );
 }
