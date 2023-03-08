f41b273206e347a08787feea9b42f0b9baa21466
RustyNova <rusty.nova.jsb@gmail.com>
2023-03-08 10:43:16 +0100

    Rework again

--- utils/calculators/Recipe/RecipeChainFinder.ts
+++ utils/calculators/Recipe/RecipeChainFinder.ts
@@ -2,50 +2,128 @@ import {DataTable} from "@/utils/classes/ORM/DataTable";
 import {RecipeDataTable} from "@/utils/ORMEntities/Recipes/RecipeDataTable";
 import {MaterialInventory} from "@/utils/ORMEntities/Materials/inventory/MaterialInventory";
 import {RecipeChainFinderStep} from "@/utils/calculators/Recipe/RecipeChainFinderStep";
-import {RecipeInventory} from "@/utils/ORMEntities/Recipes/inventory/RecipeInventory";
-import {CollectionOfUnique} from "@/utils/classes/CollectionOfUnique";
+import {RecipeChainTable} from "@/utils/ORMEntities/Recipes/inventory/RecipeChainTable";
+import {IdChecklist} from "@/utils/classes/ORM/IdChecklist";
+import {RecipeChain} from "@/utils/ORMEntities/Recipes/inventory/RecipeChain";
+import {ListOfBest} from "@/utils/classes/ORM/listOfBest";
+import _ from "lodash";
+import {ManyToMany} from "@/utils/classes/ORM/ManyToMany";
+import {RecipeChainCostCategoryTable} from "@/utils/calculators/Recipe/RecipeChainCostCategory";
+import {OneToMany} from "@/utils/classes/ORM/OneToMany";
 
 export class RecipeChainFinder extends DataTable<RecipeChainFinderStep> {
-    /** All the steps that aren't explored yet */
-    private stepsToExplore = new CollectionOfUnique<string>();
-    /** All the steps that have a completed recipe chain */
-    private stepsWithCompleteChain = new CollectionOfUnique<string>();
+    public readonly availableRecipes: RecipeDataTable;
+    public readonly parentChildrenSteps = new ManyToMany();
+    public processCounter = 0;
+    public readonly recipeChain_primaryMaterialCategory = new OneToMany<RecipeChainFinder, RecipeChainCostCategoryTable>(this, new RecipeChainCostCategoryTable());
+    private readonly _completedChainList = new IdChecklist();
+    private readonly _endedChains = new IdChecklist();
+    private readonly _toppestEndOfChains = new ListOfBest();
+    private readonly allRecipeChains = new RecipeChainTable();
+    private readonly bestCompletedChains = new ListOfBest();
+    private skippedCount = 0;
+    private readonly startGenTime = performance.now();
     /** The inventory wanted */
-    private targetInventory: MaterialInventory;
+    private readonly targetInventory: MaterialInventory;
+    private readonly unexploredChains = new IdChecklist();
 
-    constructor(recipeTable: RecipeDataTable, targetInventory: MaterialInventory) {
+    constructor(availableRecipes: RecipeDataTable, targetInventory: MaterialInventory) {
         super();
-        this.targetInventory = targetInventory
-        this.insert(new RecipeChainFinderStep(this.targetInventory, new RecipeInventory(recipeTable)))
+        this.availableRecipes = availableRecipes;
+        this.targetInventory = targetInventory;
+
+        this.bestCompletedChains.isWorse = (idBase, otherId) => this.getOrThrow(idBase).isWorse(this.getOrThrow(otherId));
+        this._toppestEndOfChains.isWorse = (idBase, otherId) => this.getOrThrow(idBase).isChildOf(this.getOrThrow(otherId));
+
+        //Create the first step
+        this.allRecipeChains.insert(new RecipeChain(this.availableRecipes));
+        this.unexploredChains.set("", true);
+        this.getOrCreate("");
     }
 
-    generateStepsDepthFirst() {
-        while (this.stepsToExplore.length !== 0) {
-            const idCurrentStep =  this.stepsToExplore[0];
-            if(idCurrentStep === undefined) {break}
-            this.processStep(this.getOrThrow(idCurrentStep))
+    /** Return the best steps of the array */
+    get bestCompletedSteps() {
+        return this.bestCompletedChains.bests.map(id => this.getOrThrow(id));
+    }
+
+    public get completedChains() {
+        return this._completedChainList.trueValues.map(chainId => this.allRecipeChains.getOrThrow(chainId));
+    }
+
+    get endsOfChain() {
+        return this._toppestEndOfChains.bests.map(endOfChain => this.getOrThrow(endOfChain));
+    }
+
+    public create(idStep: string) {
+        this.insertOrThrow(
+            new RecipeChainFinderStep(this.targetInventory, this.allRecipeChains.getOrThrow(idStep), this));
+    }
+
+    public generateStepsDepthFirst() {
+        while (this.unexploredChains.trueValues.length !== 0) {
+            const idStep = this.unexploredChains.trueValues.at(0);
+            if (idStep === undefined) {break;}
+
+            this.processStep(idStep);
         }
     }
 
+    public getOrCreate(idStep: string) {
+        const got = this.get(idStep);
+        if (got !== undefined) {return got;}
 
-    override insert(value: RecipeChainFinderStep) {
-        super.insert(value);
-        this.updateArraysForStep(value)
+        this.create(idStep);
+        return this.getOrThrow(idStep);
+    }
+
+    public getParentStepsOf(idChain: string) {
+        return this.parentChildrenSteps.getParentIdsOf(idChain).map(idParent => this.getOrCreate(idParent));
+    }
+
+    private addNewChain(chain: RecipeChain, parentChainId: string) {
+        this.parentChildrenSteps.linkIds(parentChainId, chain.id);
+        this.allRecipeChains.insert(chain);
+        this.unexploredChains.setIfNew(chain.id, true);
+    }
+
+    private isChainChildOfEndedChain(idChain: string) {
+        const chain = this.allRecipeChains.getOrThrow(idChain);
         return this
+            ._toppestEndOfChains
+            .trueValues
+            .map(idEndedChain => this.allRecipeChains.getOrThrow(idEndedChain))
+            .some(endedRecipeChain => chain.isChildOf(endedRecipeChain));
     }
 
-    private processStep(step: RecipeChainFinderStep) {
-        console.log("Processing: ", step.toString());
+    private processStep(idStep: string) {
+        // Get or create the step
+        this.processCounter += 1;
+        this.statLog("Exploring:", idStep);
+        const step = this.getOrCreate(idStep);
 
-        for (const nextRecipesInventory of step.nextRecipesInventories) {
-            this.insert(new RecipeChainFinderStep(this.targetInventory, nextRecipesInventory))
-        }
+        this.unexploredChains.set(step.id, false);
 
-        this.updateArraysForStep(step)
+        //this._completedChainList.set(step.id, step.isComplete)
+        this.bestCompletedChains.set(step.id, step.isComplete);
+
+        //this._endedChains.set(step.id, step.isEndOfChain)
+        this._toppestEndOfChains.set(step.id, step.isEndOfChain);
+
+        // If it's a child of an ended chain, then don't process it
+        if (step.isEndOfChain) {return;}
+
+        step.getUsefulChildren().forEach(child => {this.addNewChain(child, idStep);});
+
+        //TODO: Check if searching if skipping if step is parent of uncompleted steps worth it
     }
 
-    private updateArraysForStep(step: RecipeChainFinderStep) {
-        this.stepsToExplore.addOrRemove(step.id, !step.isProcessed)
+
+    private statLog(...args: any[]) {
+        const allStepCount = this.unexploredChains.size;
+        const percentComplete = _.floor((this.processCounter / allStepCount) * 100, 4);
+        const skippedPercent = _.floor((this.skippedCount / allStepCount) * 100, 4);
+        const timeSpent = performance.now() - this.startGenTime;
+        const iPerMs = _.floor(this.processCounter / timeSpent, 4);
+        console.log(`i:${this.processCounter} - r:${this.unexploredChains.trueValues.length} - s${this.skippedCount} (${skippedPercent}%) - t:${allStepCount} (${percentComplete}%) - T${timeSpent} - i/ms:${iPerMs} -`, ...args);
     }
 }
-
--- utils/calculators/Recipe/RecipeChainFinderStep.ts
+++ utils/calculators/Recipe/RecipeChainFinderStep.ts
@@ -1,26 +1,209 @@
-import {RecipeInventory} from "@/utils/ORMEntities/Recipes/inventory/RecipeInventory";
+import {RecipeChain} from "@/utils/ORMEntities/Recipes/inventory/RecipeChain";
 import {MaterialInventory} from "@/utils/ORMEntities/Materials/inventory/MaterialInventory";
+import {RecipeChainTable} from "@/utils/ORMEntities/Recipes/inventory/RecipeChainTable";
+import {MaterialRecipe} from "@/utils/ORMEntities/Recipes/MaterialRecipe";
+import {RecipeChainFinder} from "@/utils/calculators/Recipe/RecipeChainFinder";
+import {CollectionOfUnique} from "@/utils/classes/CollectionOfUnique";
+import {constructor} from "autoprefixer";
+import {Dictionary} from "@/utils/classes/Dictionary";
+import {RecipeChainCostCategory} from "@/utils/calculators/Recipe/RecipeChainCostCategory";
 
 export class RecipeChainFinderStep {
-    private _nextRecipesInventories: RecipeInventory[] | undefined
-    private currentMatInventory: MaterialInventory
-    private recipeChain: RecipeInventory;
+    public childrenRecipeChainsIDs: string[] = [];
+    public isDeadEnd = false;
+    private _inventory: MaterialInventory | undefined;
+    private _isEndOfChain: boolean | undefined;
+    private _possibleNextRecipes: MaterialRecipe[] | undefined;
+    private _primaryInventory: MaterialInventory | undefined;
+    private recipeChainFinder: RecipeChainFinder;
+    private stepRecipeChain: RecipeChain;
+    private targetInventory: MaterialInventory;
+    private _isComplete: boolean | undefined;
+    private isWorseThanCache = new Dictionary<string, boolean>()
 
-    constructor(targetInventory: MaterialInventory, recipeInventory: RecipeInventory) {
-        this.currentMatInventory = recipeInventory.unCraftInventory(targetInventory.cloneShallow())
-        this.recipeChain = recipeInventory
+    constructor(targetInventory: MaterialInventory, recipeChain: RecipeChain, recipeChainFinder: RecipeChainFinder) {
+        this.recipeChainFinder = recipeChainFinder;
+        this.stepRecipeChain = recipeChain;
+        this.targetInventory = targetInventory;
     }
 
-    get id() {return this.recipeChain.id}
+    get parents() {
+        return this.recipeChainFinder.getParentStepsOf(this.id)
+    }
 
-    get isProcessed() {return this._nextRecipesInventories !== undefined}
+    get availableRecipes() {
+        return this.recipeChainFinder.availableRecipes;
+    }
+
+    get currentBestSteps() {
+        return this.recipeChainFinder.bestCompletedSteps;
+    }
+
+    /** Extra materials at the end of the chain */
+    get extraMaterials() {
+        return this.inventory.invertQuantities().getPositiveNonNullCounts();
+    }
+
+    get id() {return this.stepRecipeChain.id;}
+
+    get costCategory() { return new RecipeChainCostCategory(this.primaryInventory.toKeyArray())}
+
+    get idCostCategory() {this.costCategory.id}
+
+    public get inventory(): MaterialInventory {
+        if (this._inventory !== undefined) {return this._inventory;}
+        this._inventory = this.stepRecipeChain.unCraftInventory(this.targetInventory.cloneShallow());
+        return this._inventory;
+    }
+
+    /** If the inventory is just primary materials */
+    get isComplete(): boolean {
+        if(this._isComplete != undefined) {return true}
+        this._isComplete = this.getPossibleNextRecipes().length === 0;
+        return this._isComplete
+    }
+
+    get isCompletedChain() {return this.getPossibleNextRecipes().length === 0;}
+
+    /** Return true if the chain shouldn't continue */
+    public get isEndOfChain(): boolean {
+        if (this._isEndOfChain === true) {return this._isEndOfChain;}
+
+        this._isEndOfChain = this.isComplete
+            || this.hasOneParentBeingEndOfChain()
+            || this.currentBestSteps.some(bestStep => this.isWorse(bestStep));
+
+        return this._isEndOfChain;
+    }
+
+    public worseThan: string | undefined = undefined;
+
+    public isWorseThanCurrentBestSteps(): boolean {
+        return this.recipeChainFinder.bestCompletedSteps.some(bestStep => {
+            if(this.isWorse(bestStep)) {
+                this.worseThan = bestStep.id
+                return true
+            }
+        })
+    }
+
+
+    public isChildOf(maybeParentStep: RecipeChainFinderStep): boolean {
+        //TODO: Perf increase? -> Check if the step asked is a parent of this step
+        return this.stepRecipeChain.isChildOf(maybeParentStep.stepRecipeChain);
+    }
+
+    public hasOneParentBeingEndOfChain(): boolean {
+        return this.parents.some(parent => parent._isEndOfChain)
+            || this.recipeChainFinder.endsOfChain.some(endOfChain => this.isChildOf(endOfChain))
+    }
+
+    /** Return true if the step can be explored, and is worth it */
+    get isExplorable() {return !(this.isCompletedChain || this.isDeadEnd);}
+
+    /** Return true if the step has generated children */
+    get isExplored() {return this.childrenRecipeChainsIDs.length !== 0;}
+
+    /** Materials required at the start of the chain */
+    get materialsRequired() {
+        return this.inventory.getPositiveNonNullCounts();
+    }
+
+    get primaryInventory(): MaterialInventory {
+        if (this._primaryInventory !== undefined) {return this._primaryInventory;}
+
+        this._primaryInventory = this.inventory.filterKeys(this.availableRecipes.getPrimaryMaterialsId());
+        return this._primaryInventory;
+    }
+
+    /** Check if the step is a dead end. AKA, worse than a better step */
+    public checkIfDeadEnd(bestSteps: RecipeChainFinderStep[]) {
+        this.isDeadEnd = bestSteps.some(bestStep => this.isWorse(bestStep));
+        return this.isDeadEnd;
+    }
 
     /** Return all the possible Recipe Inventories */
-    get nextRecipesInventories(): RecipeInventory[] {
-        if (this._nextRecipesInventories !== undefined) {return this._nextRecipesInventories}
+    public generateChildrenRecipeChains(recipeChainTable: RecipeChainTable) {
+        const recipeAvailable = this.getPossibleNextRecipes();
+        const childrenRecipeChains = recipeAvailable.map(recipe => this.stepRecipeChain.cloneShallow().addToItemQuantity(recipe.id, 1));
 
-        const recipeAvailable = this.recipeChain.recipeTable.getRecipesThatConsume(this.currentMatInventory.getPositiveNonNullCounts().toKeyArray())
-        this._nextRecipesInventories = recipeAvailable.map(recipe => this.recipeChain.cloneShallow().addToItemQuantity(recipe.id, 1))
-        return this._nextRecipesInventories
+        recipeChainTable.insertMultiple(childrenRecipeChains);
+        this.childrenRecipeChainsIDs = childrenRecipeChains.map(recipeChain => recipeChain.id);
+        return this;
+    }
+
+    public getAllChildren() {
+        return this.getChildrenFromRecipes(this.availableRecipes.toValueArray());
+    }
+
+    public getPossibleNextRecipes() {
+        if (this._possibleNextRecipes !== undefined) {return this._possibleNextRecipes;}
+        this._possibleNextRecipes = this.availableRecipes.toValueArray_sortedByCompatibility(this.materialsRequired, this.extraMaterials);
+        return this._possibleNextRecipes;
+    }
+
+    getPrimaryMaterialsInventory() {
+        return this.inventory.filterKeys(this.availableRecipes.getPrimaryMaterialsId());
+    }
+
+    public getUsefulChildren() {
+        return this.getChildrenFromRecipes(this.getPossibleNextRecipes());
+    }
+
+    public hasMorePrimaryMaterials(otherStep: RecipeChainFinderStep) {
+        // You can only compare to a completed chain
+        if(!otherStep.isCompletedChain){return false}
+
+        // All the material must have the exact same count or more
+        if (!this.primaryInventory.isSuperiorOrEqual(otherStep.primaryInventory)) {return false}
+
+        // The primary material must also not be the same
+        return !this.primaryInventory.isEqual(otherStep.primaryInventory);
+    }
+
+    public hasMoreStepsForSameInventory(otherStep: RecipeChainFinderStep) {
+        // It must have more steps
+        if (this.stepRecipeChain.totalNumberOfItems < otherStep.stepRecipeChain.totalNumberOfItems) {return false}
+
+        // The inventory must be equal
+        return this.inventory.isEqual(otherStep.inventory)
+    }
+
+    public hasMoreStepsForTheSameFinalPrimaryMaterials(otherStep: RecipeChainFinderStep) {
+        // It must be a completed chain
+        if(!this.isCompletedChain) {return false}
+
+        // The step compared against must be a completed chain
+        if(!otherStep.isCompletedChain) {return false}
+
+        // It must have more steps
+        if(!(this.stepRecipeChain.totalNumberOfItems > otherStep.stepRecipeChain.totalNumberOfItems)) {return false}
+
+        // It must be equal
+        return this.primaryInventory.isEqual(otherStep.primaryInventory);
+    }
+
+    public isWorse(otherStep: RecipeChainFinderStep) {
+        const cacheHit = this.isWorseThanCache.get(otherStep.id);
+        if(cacheHit !== undefined) {return cacheHit}
+
+        const thisInvID = this.getPrimaryMaterialsInventory().id;
+        const otherInvID = otherStep.getPrimaryMaterialsInventory().id;
+
+        if(this.id === otherStep.id) {return false}
+
+        if(this.hasMorePrimaryMaterials(otherStep)) {return true}
+
+        if(this.hasMoreStepsForSameInventory(otherStep)) {return true}
+
+        return this.hasMoreStepsForTheSameFinalPrimaryMaterials(otherStep);
+    }
+
+    toString() {
+        return `Recipe Chain: ${this.stepRecipeChain.id}, Inventory: ${this.inventory.id}`;
+    }
+
+    private getChildrenFromRecipes(recipes: MaterialRecipe[]) {
+        return recipes.map(recipe => this.stepRecipeChain.cloneShallow().addToItemQuantity(recipe.id, 1));
     }
 }
